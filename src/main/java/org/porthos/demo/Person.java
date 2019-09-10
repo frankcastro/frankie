30c7ce0eb38d858acd6571b287a0dba46b916dd3
Frank Castro <frank.castro@cdk.com>
2019-09-10 14:41:57 -0400

    Person class

---
+++ src/main/java/org/porthos/demo/Person.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.porthos.demo;
+
+import lombok.Data;
+
+/**
+ *
+ * @author castrof
+ */
+@Data
+public class Person {
+    
+    private String lastName;
+    private String firstName;
+    
+}
